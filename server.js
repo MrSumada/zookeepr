d4404cdf34a7550dd5cff995590084e3d6263f1a
MrSumada <ricksamericana.com>
2022-02-21 20:14:52 -0500

    add Heroku

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const { animals } = require("./data/animals.json")
+
+app.get("/api/animals", (req, res) => {
+    let results = animals;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+});
+
+function filterByQuery(query, animalsArray) {
+
+    let personalityTraitsArray = [];
+    let filteredResults = animalsArray;
+
+    if (query.personalityTraits) {
+        if (typeof query.personalityTraits === "string") {
+            personalityTraitsArray = [query.personalityTraits];
+        } else {
+            personalityTraitsArray = query.personalityTraits;
+        }
+
+        // For Each checks EVERY personality trait, if it isn't present 
+        // (aka indexOf yields -1), then it's removed
+        // Only animals with each trait requested will remain unfiltered 
+        personalityTraitsArray.forEach(trait => {
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            );
+        });
+    }
+
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+
+    return filteredResults;
+}
